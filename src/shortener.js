6d2cda56b0c435272d76e84375c173b5f724cb41
Laura Brandt <laurambrandt@gmail.com>
2017-06-09 18:49:31 +0000

    cleanup

--- src/shortener.js
+++ src/shortener.js
@@ -1,19 +1,27 @@
 'use strict';
 
-var functions = require('./shortener_funcs.js');
 var validUrl = require('valid-url');
 
 module.exports = function(req, res) {
     var orig_url = req.params[0];
-    console.log(orig_url)
     if (validUrl.isUri(orig_url)) {
         do {
-            var urlID = functions.LPad(Math.floor(Math.random()*10000), 4);
+            var urlID = LPad(Math.floor(Math.random()*10000), 4);
             var short_url = req.headers.host + '/' + urlID;
         } while (false); //TODO - check to make sure it doesn't already exist in db
-        res.write('new url is: ' + short_url);
+        //TODO - add to db
+        res.end(JSON.stringify(
+                    { 'Shortened URL' : short_url,
+                      'Original URL' : orig_url },
+                    null,
+                    2));
     } else {
         res.end(JSON.stringify({'Error': 'Sorry, that is not a valid URL. Please check the URL and try again.'}, null, 2));
     }
-    res.end();  
+};
+
+var LPad = function(num, length) {
+        var str = num.toString();
+        while (str.length < length) str = '0' + str;
+        return str;
 };
