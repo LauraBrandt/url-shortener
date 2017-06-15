b0385c08d37036791467180be0a6f5e4b88c27f7
Laura Brandt <laurambrandt@gmail.com>
2017-06-15 17:35:51 +0000

    cleaned things up

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var routes = require('./src/routes.js');
 
 // Set up for running
-let port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 
 var app = express();
 
--- src/routes.js
+++ src/routes.js
@@ -4,23 +4,23 @@ var shortener = require('./shortener.js');
 var redirect = require('./redirect.js');
 
 module.exports = function(app) {
-    app.get("/", (req, res) => {
+    app.get("/", function(req, res) {
       res.sendFile(process.cwd() + "/public/index.html");
     });
     
-    app.get('/new/*', (req, res) => {
+    app.get('/new/*', function(req, res) {
       console.log('Creating new URL ...');
       res.writeHead(200, { "Content-Type": "text/plain" });
       shortener(req, res);
     });
     
-    app.get('/:shortid', (req, res) => {
+    app.get('/:shortid', function(req, res) {
       console.log('Getting original URL ...');
       res.writeHead(200, { "Content-Type": "text/plain" });
       redirect(req, res);
     });
     
-    app.get('*', (req, res) => {
+    app.get('*', function(req, res) {
       res.writeHead(404, { "Content-Type": "text/plain" });
       res.end(JSON.stringify({"Error": "404"}));
     });
